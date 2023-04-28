15bce536e59557d0ccf2ed87dcbb5181c49c22b7
zhujinle <32472522+zhujinle@users.noreply.github.com>
2023-04-28 23:31:47 +0800

    定义了部分model
    讲实话，和word有点出入，到时候改word

--- 1_code/background/background/settings.py
+++ 1_code/background/background/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # app导入
+    'databaseManagementLocal.apps.DatabasemanagementlocalConfig',
 ]
 
 MIDDLEWARE = [
--- 1_code/background/databaseManagementLocal/models.py
+++ 1_code/background/databaseManagementLocal/models.py
@@ -1,8 +1,36 @@
 from django.db import models
 
 # Create your models here.
-class Customer(models.Model)
+class User(models.Model):
     # 名儿
     name = models.CharField(max_length=20)
     # 联系方式
     phone = models.CharField(max_length=20)
+    # UID
+    UID = models.AutoField()
+    # 用户类型标记
+    class UserType(models.IntegerChoices):
+        Customer = 1
+        Seller = 2
+        DeliveryStaff = 3
+        Admin = 4
+    type = models.IntegerField(choices = UserType)
+    # 会员等级
+    class MenberType(models.IntegerChoices):
+        Cu = 1
+        Ag = 2
+        Au = 3
+        diamond = 4
+    Member = models.IntegerField(choices = MenberType)
+    # 地址
+    Address = models.CharField(max_length=100)
+    # 日金额，在用户侧定义为日消费金额，商家侧定义为日总收入，配送测定义为日收入，一日一清零
+    MoneyMonthly = models.IntegerField(max_length=10)
+    # 金额，在用户侧定义为月消费金额，商家侧定义为月总收入，配送测定义为月收入，一月一清零
+    MoneyMonthly = models.IntegerField(max_length=10)
+    # 总金额
+    MoneySum = models.IntegerField(max_length=40)
+
+class Cart(models.Model)
+    # UID
+    uid = models.ForeignKey('User.uid',on_delete=models.CASCADE,)
